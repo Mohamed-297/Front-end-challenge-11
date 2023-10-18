01be79cea99eec2aba07b13d2eb15c2ea805ac11
mohamed <mohamedelsherif671@gmail.com>
2023-10-18 16:21:27 +0300

    Remove the undefined

--- script.js
+++ script.js
@@ -23,15 +23,7 @@ function themeChange(){
         monitor.style.cssText="background-color:hsl(224, 36%, 15%);color:white;"
         buttonsContainer.style.cssText="background-color:#232c43"
         themeOneBtnColor();
-        // buttons.forEach(btn=>{
-
-        //     btn.style.cssText="background-color:white;color:hsl(224, 36%, 15%);box-shadow: 0px 3.3px 0px 0px hsl(0, 0%, 70%)"
-        // }
-        // )
-        // clearBtn.style.cssText="background-color:hsl(225, 21%, 49%);box-shadow: 0px 3.3px 0px 0px hsl(224, 28%, 35%);"
-        // deleteBtn.style.cssText="background-color:hsl(225, 21%, 49%);box-shadow: 0px 3.3px 0px 0px hsl(224, 28%, 35%);"
-        // equalBtn.style.cssText="background-color:hsl(6, 63%, 50%);box-shadow: 0px 3.3px 0px 0px hsl(6, 70%, 34%)"
-    }
+        }
     else if(themeValue==2){
         ball.style.cssText="left:25px;position:relative;background-color:hsl(25, 98%, 40%);"
         themes.style.cssText="background-color:hsl(45, 7%, 89%);"
@@ -39,9 +31,6 @@ function themeChange(){
         headerContent.style.cssText="color:black"
         monitor.style.cssText="background-color:white;color:black;"
         buttonsContainer.style.cssText="background-color:hsl(45, 7%, 89%)"
-        // clearBtn.style.cssText="background-color:hsl(185, 42%, 37%);box-shadow: 0px 3.3px 0px 0px hsl(185, 58%, 25%);"
-        // deleteBtn.style.cssText="background-color:hsl(185, 42%, 37%);box-shadow: 0px 3.3px 0px 0px hsl(185, 58%, 25%);"
-        // equalBtn.style.cssText="background-color:hsl(25, 98%, 40%);box-shadow: 0px 3.3px 0px 0px  hsl(25, 99%, 27%);"
         themeTwoBtnColor()
     }
     else if(themeValue==3){
@@ -52,15 +41,6 @@ function themeChange(){
         monitor.style.cssText="background-color:hsl(268, 71%, 12%);color:hsl(52, 100%, 62%);"
         buttonsContainer.style.cssText="background-color:hsl(268, 71%, 12%)"
         themeThreeBtnColor()
-        // buttons.forEach(btn=>{
-
-        //     btn.style.cssText="background-color:hsl(268, 47%, 21%);color:hsl(52, 100%, 62%);box-shadow: 0px 3.3px 0px 0px hsl(290, 70%, 36%)"
-        // }
-        // )
-        // clearBtn.style.cssText="background-color:hsl(281, 89%, 26%);box-shadow: 0px 3.3px 0px 0px hsl(285, 91%, 52%);"
-        // deleteBtn.style.cssText="background-color:hsl(281, 89%, 26%);box-shadow: 0px 3.3px 0px 0px hsl(285, 91%, 52%);"
-        // equalBtn.style.cssText="background-color:hsl(176, 100%, 44%);color:black;box-shadow: 0px 3.3px 0px 0px  hsl(177, 92%, 70%);"
-
     }
     temp=themeValue
     themeValue=( themeValue % 3 );
@@ -116,8 +96,6 @@ function themeThreeBtnColor(){
     clearBtn.style.cssText="background-color:hsl(281, 89%, 26%);box-shadow: 0px 3.3px 0px 0px hsl(285, 91%, 52%);"
     deleteBtn.style.cssText="background-color:hsl(281, 89%, 26%);box-shadow: 0px 3.3px 0px 0px hsl(285, 91%, 52%);"
     equalBtn.style.cssText="background-color:hsl(176, 100%, 44%);color:black;box-shadow: 0px 3.3px 0px 0px  hsl(177, 92%, 70%);"
-
-
 }
 
 buttons.forEach(btn=>{
@@ -135,6 +113,7 @@ buttons.forEach(btn=>{
         }
     })
 })
+
 clearBtn.addEventListener("click",()=>{
     monitorText.innerHTML=""
 })
@@ -142,10 +121,14 @@ deleteBtn.addEventListener("click",()=>{
     monitorText.innerHTML=monitorText.innerHTML.substring((monitorText.innerHTML.length)-1,"")
     
 })
-equalBtn.addEventListener("click",()=>{
 
+equalBtn.addEventListener("click",()=>{
 let res =eval(monitorText.innerHTML)    
-monitorText.innerHTML=res
-res=0
-    
+if(res!=null){
+    monitorText.innerHTML=res
+}
+else{
+    monitorText.innerHTML="0"
+}
+res=0  
 })
